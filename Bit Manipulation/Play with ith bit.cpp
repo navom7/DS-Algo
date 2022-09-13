c010ef1d99e63156b068c0ad482d53ae2738c160
NANHE KUMAR PANDEY <36000529+navom7@users.noreply.github.com>
2022-09-13 23:57:15 +0530

    Create Play with ith bit.cpp

---
+++ Bit Manipulation/Play with ith bit.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int getIthBit(int n, int i){
+    int mask = (1 << i);
+    if(n & mask){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+void setIthBit(int &n, int i){
+    int mask = (1 << i);
+    n = n | mask;
+}
+
+void clearIthBit(int &n, int i){
+    int mask = (1<<i);
+    mask = ~mask;
+    n = n&mask;
+}
+
+void UpdateIthBitWithValue(int &n, int i, int v){
+    clearIthBit(n, i);
+    int mask = (v<<i);
+    n = (n|mask);
+}
+void ClearLastiBits(int &n, int i){
+    int mask = (-1 << i);
+    n = n&mask;
+}
+
+int main() {
+    int n, i, v;
+    cin >> n >> i >> v;
+    cout << getIthBit(n, i)<< endl;
+    setIthBit(n, i);
+    cout << n << endl;
+    clearIthBit(n, i);
+    cout << n << endl;
+    cout << n << endl;
+    
+    UpdateIthBitWithValue(n, i, v);
+    
+    ClearLastiBits(n, i);
+    
+    cout << n << endl;
+}
