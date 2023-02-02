c7cbf497c0323c7506e42ab43df2ed5c4a69e203
navom7 <navo.akp@gmail.com>
2023-02-03 00:52:04 +0530

    n ladder optimized dp approach

--- dynamic_programming/n_ladder.cpp
+++ dynamic_programming/n_ladder.cpp
@@ -28,36 +28,33 @@ int n_ladder_bottom_up(int n, int k){
     int arr[n+1] = {0};
     arr[0] = 1; 
     for(int i = 1; i <= n; i++){
-        int idx = 1;
-        int temp = 0;
-        while(idx <= k && (i-idx) >= 0){
-            temp += arr[i-idx];
-            idx++;
+        for(int jump = 1; jump <= k; jump++){
+            if(i-jump >= 0){
+                arr[i] += arr[i-jump];
+            }
         }
-        // cout << i << " " << temp << endl;
-        arr[i] = temp;
     }
     return arr[n];
 }
 
 
 
-int n_ladder_bottom_up(int n, int k){
+int n_ladder_bottom_up_optimized(int n, int k){
     if(n < 0)
         return 0;
-    int arr[n+1] = {0};
-    arr[0] = 1; 
-    for(int i = 1; i <= n; i++){
-        int idx = 1;
-        int temp = 0;
-        while(idx <= k && (i-idx) >= 0){
-            temp += arr[i-idx];
-            idx++;
+    int dp[n+1] = {0};
+    dp[0] = 1; 
+
+    for(int i = 1; i <= k; i++){
+        for(int idx = 0; idx < i; idx++){
+            dp[i] += dp[idx];
         }
-        // cout << i << " " << temp << endl;
-        arr[i] = temp;
     }
-    return arr[n];
+
+    for(int i = k+1; i <= n; i++){
+        dp[i] = 2*dp[i-1] - dp[n-k-1];
+    }
+    return dp[n];
 }
 
 
@@ -68,6 +65,7 @@ int main(){
     cout <<"N Ladder n_ladder_top_down ===========================> " << n_ladder_top_down(n, k, dp) << endl;
     
     cout <<"N Ladder n_ladder_bottom_up ===========================> " << n_ladder_bottom_up(n, k) << endl;
+    cout <<"N Ladder n_ladder_bottom_up ===========================> " << n_ladder_bottom_up_optimized(n, k) << endl;
 
     return 0;
 }
