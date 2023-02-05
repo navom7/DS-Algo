84c4924e04012d636cdbed30bffd77d04499dd95
NANHE KUMAR PANDEY <36000529+navom7@users.noreply.github.com>
2023-02-05 19:42:15 +0530

    Update rod_cutting.cpp

--- recursion/rod_cutting.cpp
+++ recursion/rod_cutting.cpp
@@ -18,25 +18,50 @@ a length 2 and 6
 #include<bits/stdc++.h>
 using namespace std;
 
-int cutting_rod(vector<int> &vec, int len){
-    cout << len << " ";
+int cutting_rod_top_down(vector<int> &vec, int len, vector<int> & dp){
+    if(dp[len] != 0) {
+        return dp[len];
+    }
     if(len <= 0){
         return 0;
     }
+    // cout << len << endl;
     int price = INT_MIN;
     for(int i = 0; i < len; i++){
-        int temp_price = vec[i] + cutting_rod(vec, len-i-1);
+        int temp_price = vec[i] + cutting_rod_top_down(vec, len-i-1, dp);
         price = max(price, temp_price);
     }
-    return price;
+    return dp[len] = price;
 }
+
+int rod_cutting_bottom_up(vector<int> & vec, int len) {
+    vector<int> dp(len+1, 0);
+    if(len == 0) {
+        return 0;
+    }
+    for(int i = 1; i <= len; i++) {
+        for(int idx = 1; idx <= i; idx++) {
+            int temp = dp[i-idx] + vec[idx-1];
+            cout << temp << ", " << vec[i-1] << " => ";
+            dp[i] = max(temp, dp[i]);
+            cout << i << " " << dp[i] << endl;
+        }
+        
+    }
+    return dp[len];
+}
+
+
+
 
 int main(){
     int len;
     vector<int> vec = {1, 5, 8, 9, 10, 17, 17, 20};
     len = vec.size();
-    cout << "output: " << cutting_rod(vec, len) << endl;
+    vector<int> dp(len, 0);
+    cout << "top down output: " << cutting_rod_top_down(vec, len, dp) << endl;
 
+    cout << "bottom up output: " << rod_cutting_bottom_up(vec, len) << endl;
 
     return 0;
 }
