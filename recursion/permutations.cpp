cb28b45ef4ba8dcde687093e9cf744bf89c5cd4a
navom7 <navo.akp@gmail.com>
2023-02-16 00:48:47 +0530

    permutations of a string

---
+++ recursion/permutations.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printPermutations(string &str, int l, int r) {
+    if(l == r){
+        cout << str << endl;
+    } else {
+        for(int i = l; i <= r; i++) {
+            swap(str[l], str[i]);
+            printPermutations(str, l+1, r);
+            swap(str[l], str[i]);
+        }
+    }
+}
+
+
+int main() {
+    string str = "abc";
+    int n = str.size();
+    printPermutations(str, 0, n-1);
+    
+
+    return 0;
+}
