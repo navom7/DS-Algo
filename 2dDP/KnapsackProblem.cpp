786430d9148026a9713a9890821bc20e827ff3b3
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-01-08 23:36:26 +0530

    Recursive Code Top Down DP

--- 2dDP/KnapsackProblem.cpp
+++ 2dDP/KnapsackProblem.cpp
@@ -3,6 +3,22 @@
 
 using namespace std;
 
+//Recursive Code -> Top Down
+int knapsack(int wts[], int prices[], int N, int W, vector<vector<int>> &dp) {
+    if(N <= 0 || W <= 0) {
+        return 0;
+    }
+    if(dp[N][W] != 0) {
+        return dp[N][W];
+    }
+    int a = 0;
+    if((W-wts[N-1]) > 0) {
+        a = prices[N-1] + knapsack(wts, prices, N-1, W-wts[N-1], dp);
+    }
+    int b = knapsack(wts, prices, N-1, W, dp);
+
+    return dp[N][W] = max(a, b);
+}
 
 
 int main() {
@@ -12,7 +28,8 @@ int main() {
     int N = 4;
     int W = 11;
 
-    cout << knapsack(wts, prices, N, W) << endl;
+    vector<vector<int>> dp(N+1, vector<int>(W + 1, 0));
+    cout << knapsack(wts, prices, N, W, dp) << endl;
 
     return 0;
 }
