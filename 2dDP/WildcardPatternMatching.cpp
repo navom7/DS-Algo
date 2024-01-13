9ddf64815a0b0e8427326fb340ec817cb417dadb
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-01-13 20:44:17 +0530

    pattern matching

---
+++ 2dDP/WildcardPatternMatching.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool patternMatchingHelper(string pattern, string input, int i, int j) {
+    int ps = pattern.size();
+    int is = input.size();
+    if(i == ps-1) {
+        if(pattern[i] == "*") {
+            return true;
+        } else if(pattern[i] == '?') {
+            if(j == is-1) {
+                return true;
+            } else {
+                return false;
+            }
+        } 
+
+
+    }
+}
+
+bool patternMatching(string s, string p){
+     
+    
+}
+
+
+
+
+int main() {
+    string input = "baaabab";
+    string pattern = "*****ba*****ab";
+
+    cout << patternMatching(input, pattern) << endl;
+}
