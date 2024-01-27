f542e6e904fd7b8f35630e80fe9634539c2a232b
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-01-27 17:42:25 +0530

    is valid bfs udemy problem

--- UdemyGraph/IsValidBfs.cpp
+++ UdemyGraph/IsValidBfs.cpp
@@ -5,17 +5,50 @@ using namespace std;
 
 bool validBfs(int n, vector<int> sequence , vector<vector<int>> edges)
 {
-    vector<vector<int>> lst(n+1);
-    
 
-
-
-    for(auto x: lst) {
-        for(auto y: x) {
-            cout << y << " ";
-        }
-        cout << endl;
+    if(sequence.size() < n) {
+        return false;
     }
+    if(n < 2) {
+        return false;
+    }
+
+    vector<bool> visited(n+1, false);
+    vector<vector<int>> lst(n+1);
+    vector<int> level(n+1, 0);
+
+    for(auto x: edges) {
+        lst[x[0]].push_back(x[1]);
+        lst[x[1]].push_back(x[0]);
+    }
+
+
+
+    queue<int> q;
+    q.push(1);
+    visited[1] = true;
+
+    while(!q.empty()) {
+        int frnt = q.front();
+        q.pop();
+
+        for(auto x: lst[frnt]) {
+            if(!visited[x]) {
+                q.push(x);
+                level[x] = level[frnt]+1;
+                visited[x] = true;
+            }
+        }
+    }
+
+    for(int i = 1; i < n; i++) {
+        int curr = level[sequence[i]];
+        int prev = level[sequence[i-1]];
+
+        if(curr < prev)
+            return false;
+    }
+
     return true;
 }
 
@@ -28,7 +61,7 @@ int main() {
         {5, 12}, {5, 11}, {5, 10}
     };
 
-    vector<int> sequence = {1,2,3,4,6,5, 9, 8, 7, 12, 11, 10};
+    vector<int> sequence = {1,2,3, 10,4,6,5, 9, 8, 7, 12, 11};
 
     int n = 12;
 
