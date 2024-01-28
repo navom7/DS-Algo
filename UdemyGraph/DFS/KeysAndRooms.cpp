07e6c6f8901926f894112d6968f0978d0862999f
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-01-28 10:00:12 +0530

    added problem statement

--- UdemyGraph/DFS/KeysAndRooms.cpp
+++ UdemyGraph/DFS/KeysAndRooms.cpp
@@ -1,3 +1,30 @@
+/*
+Keys and Rooms
+There are N rooms and you start in room 0. Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may have some keys to access the next room.
+
+Formally, each room i has a list of keys rooms[i], and each key rooms[i][j] is an integer in [0, 1, ..., N-1] where N = rooms.length. A key rooms[i][j] = v opens the room with number v.
+
+Initially, all the rooms start locked (except for room 0).
+
+You can walk back and forth between rooms freely.
+
+Return true if and only if you can enter every room.
+
+Constraints:
+
+1 <= rooms.length <= 1000
+
+0 <= rooms[i].length <= 1000
+
+The number of keys in all rooms combined is at most 3000.
+
+
+
+Expected Time Complexity: O(N + E), where N is the number of rooms, and E is the total number of keys.
+*/
+
+
+
 #include<iostream.h>
 #include<vector>
 #include<list>
