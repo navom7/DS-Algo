4e7050f03ed7dcd23994c6e8bf79d691e1d73343
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-02-05 23:22:18 +0530

    2433. Find The Original Array of Prefix Xor

---
+++ BitManipulation/FindOriginalArrayofPrefixXor.cpp
@@ -0,0 +1,12 @@
+//2433. Find The Original Array of Prefix Xor
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        vector<int> res;
+        res.push_back(pref[0]);
+        for(int i = 1; i < pref.size(); i++) {
+            res.push_back(pref[i]^pref[i-1]);
+        }
+        return res;
+    }
+};
